af82711a109c727547902344e01bae7507ec9bcc
kizailent <egami.t.ab@m.titech.ac.jp>
2025-03-17 08:53:40 +0000

    [Finsh] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :index, :show, :create, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
 
   devise_for :users
